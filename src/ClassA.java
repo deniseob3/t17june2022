21786e5785eddebf65b7e67a06cbe45ddd812ab0
deniseob3 <obried40@tcd.ie>
2022-06-17 20:28:05 +0100

    commit eighteen

--- src/ClassA.java
+++ src/ClassA.java
@@ -1,9 +1,9 @@
-package com.example.pack
-
-
 public class ClassA {
     public static void main(String [] args)
     {
+       int eighteen = 8 + 10;
+
+
         System.out.print("Success");
     }
 }
